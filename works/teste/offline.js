196f3e62e249ea83a4ced06dde07863656761326
guimaraf <guimaraf@gmail.com>
2018-10-08 22:00:40 -0300

    update pointer

--- works/teste/offline.js
+++ works/teste/offline.js
@@ -1,12 +1,12 @@
 ﻿{
-	"version": 1538681246,
+	"version": 1539046759,
 	"fileList": [
 		"data.js",
 		"c2runtime.js",
 		"jquery-2.1.1.min.js",
 		"offlineClient.js",
 		"images/soul-sheet0.png",
-		"images/sprite-sheet0.png",
+		"images/target-sheet0.png",
 		"icon-16.png",
 		"icon-32.png",
 		"icon-114.png",
