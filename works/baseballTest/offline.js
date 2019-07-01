0dd92ba6fecd2d8ec4f735a250c16e678f16e1ee
guimaraf <guimaraf@gmail.com>
2019-07-01 13:57:28 -0300

    new game test

---
+++ works/baseballTest/offline.js
@@ -0,0 +1,24 @@
+﻿{
+	"version": 1562000142,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/soul-sheet0.png",
+		"images/wall.png",
+		"images/mario-sheet0.png",
+		"images/collider-sheet0.png",
+		"images/ball-sheet0.png",
+		"images/camera-sheet0.png",
+		"images/bat-sheet0.png",
+		"images/bat-sheet1.png",
+		"images/spritefont.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
