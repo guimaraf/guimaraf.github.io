1dd08e5b8b42ed3a3c8e5ae80046a06e22d98602
guimaraf <guimaraf@gmail.com>
2019-07-26 11:24:47 -0300

    add challenge

---
+++ works/stage/offline.js
@@ -0,0 +1,23 @@
+﻿{
+	"version": 1564151056,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/coins-sheet0.png",
+		"images/back-sheet0.png",
+		"images/board_select-sheet0.png",
+		"images/buttonlevel-sheet0.png",
+		"images/buttonlevel-sheet1.png",
+		"images/btn_clear_levels-sheet0.png",
+		"images/soul-sheet0.png",
+		"images/tiledbackground.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
