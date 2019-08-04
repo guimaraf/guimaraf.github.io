6161a8fb2a354bb49b8f2789a2608970709f1fd2
guimaraf <guimaraf@gmail.com>
2019-08-04 13:09:35 -0300

    add game prototype

---
+++ works/folsen/offline.js
@@ -0,0 +1,29 @@
+﻿{
+	"version": 1564934905,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/soul-sheet0.png",
+		"images/folsen-sheet0.png",
+		"images/folsen-sheet1.png",
+		"images/ground.png",
+		"images/enemy-sheet0.png",
+		"images/jar-sheet0.png",
+		"images/jar-sheet1.png",
+		"images/collider-sheet0.png",
+		"media/explosion.m4a",
+		"media/explosion.ogg",
+		"media/jump.m4a",
+		"media/jump.ogg",
+		"media/attack1.m4a",
+		"media/attack1.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
