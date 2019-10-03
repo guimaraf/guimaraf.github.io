eadd5d5ac3069eb5664f109dba60053b62ee4a2b
guimaraf <guimaraf@gmail.com>
2019-10-03 18:15:23 -0300

    update aboult

---
+++ assets/js/htmlToImage.js
@@ -0,0 +1,12 @@
+function downloadimage(){
+    //alert(document.getElementById("ww"));
+    var container = document.getElementById("htmltoimage"); // full page 
+    html2canvas(container,{allowTaint : true}).then(function(canvas) {
+        var link = document.createElement("a");
+        document.body.appendChild(link);
+        link.download = "html_image.jpg";
+        link.href = canvas.toDataURL();
+        link.target = '_blank';
+        link.click();
+    });
+}
