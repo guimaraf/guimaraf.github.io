4cbeac63c1cda7abbec2f5de428aadce5ba20f77
guimaraf <guimaraf@gmail.com>
2020-03-06 10:11:08 -0300

    add games

---
+++ works/clock/offline.js
@@ -0,0 +1,24 @@
+﻿{
+	"version": 1566400508,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/digitalhour.png",
+		"images/timerbg-sheet0.png",
+		"images/hour-sheet0.png",
+		"images/minuts-sheet0.png",
+		"images/seconds-sheet0.png",
+		"images/mile-sheet0.png",
+		"images/tiledbackground.png",
+		"media/clock.m4a",
+		"media/clock.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
---
+++ works/inventory/offline.js
@@ -0,0 +1,25 @@
+﻿{
+	"version": 1549980889,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/item-sheet0.png",
+		"images/item-sheet1.png",
+		"images/item-sheet2.png",
+		"images/txtqtditens.png",
+		"images/invisibleselect-sheet0.png",
+		"images/select-sheet0.png",
+		"images/select-sheet1.png",
+		"images/infoitem.png",
+		"images/inventorybg-sheet0.png",
+		"images/bg-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
