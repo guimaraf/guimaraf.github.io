eacb97cf49fbe82459d8b3644614957999d82075
guimaraf <guimaraf@gmail.com>
2020-07-18 19:51:57 -0300

    test json

---
+++ js/play.js
@@ -0,0 +1,10 @@
+let songs = {
+  "Test1" : {
+    url : "files/Boing_Boing.webm"
+  },
+  "Test2" : {
+    url : "files/gamesaved.wav"
+  }
+}
+
+playSound= (name) => new Audio(songs[name].url).play()
