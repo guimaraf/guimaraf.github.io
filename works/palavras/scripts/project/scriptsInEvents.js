ceb358b5330871fbd55cb96665ea87a14bdec1be
guimaraf <guimaraf@gmail.com>
2022-04-22 13:16:09 -0300

    add game palavras

---
+++ works/palavras/scripts/project/scriptsInEvents.js
@@ -0,0 +1,15 @@
+
+
+
+const scriptsInEvents = {
+
+		async Menus_Event8_Act2(runtime, localVars)
+		{
+			//window.open("https://www.w3schools.com");
+			window.open("https://www.w3schools.com", "_self");
+		}
+
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
+
