6db5029426f89f9b2b93c6687dc76757fa505fe1
guimaraf <guimaraf@gmail.com>
2023-02-26 18:42:20 -0300

    add sounds

---
+++ works/planets/scripts/project/scriptsInEvents.js
@@ -0,0 +1,19 @@
+
+
+
+const scriptsInEvents = {
+
+	async Common_Event45_Act2(runtime, localVars)
+	{
+		console.log('roda musica menu')
+	},
+
+	async Common_Event47_Act2(runtime, localVars)
+	{
+		console.log('roda musica gameplay')
+	}
+
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
+
