9bae090c61ecc49c4dd1801d716ec462ec6d40c5
guimaraf <guimaraf@gmail.com>
2023-04-11 15:37:51 -0300

    add new game squab alpha

---
+++ works/squabgame/scripts/project/scriptsInEvents.js
@@ -0,0 +1,34 @@
+
+
+
+const scriptsInEvents = {
+
+	async Character_Event86_Act4(runtime, localVars)
+	{
+		console.log("caralkha")
+	},
+
+	async Comum_Event88_Act2(runtime, localVars)
+	{
+		console.log('registered new player')
+	},
+
+	async Comum_Event90_Act2(runtime, localVars)
+	{
+		console.log('Record update')
+	},
+
+	async Comum_Event91_Act1(runtime, localVars)
+	{
+		console.log('lower score, not sent')
+	},
+
+	async Comum_Event92_Act3(runtime, localVars)
+	{
+		console.log('aguardou o tempo')
+	}
+
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
+
