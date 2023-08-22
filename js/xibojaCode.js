77d036804375db8caa40620033ee90c8fe0c9c66
guimaraf <guimaraf@gmail.com>
2023-08-22 12:21:02 -0300

    add export c3 icons

--- js/xibojaCode.js
+++ js/xibojaCode.js
@@ -1,26 +1,29 @@
 const squareResolutions = [[44,44], [71,71], [75,75], [88,88], [100,100], [142,142], [150,150], [176,176], [200,200], [256,256], [284,284], [300,300], [310,310], [600,600], [620,620], [1240,1240]];
 const rectangularResolutions = [[310,150], [620,300], [930,450], [1240,600], [2480, 1200]];
+const construct3Icons = [[16,16],[32,32],[64,64],[128,128],[256,256],[512,512]];
 
 function resizeImage(image, squareResolutions, rectangularResolutions) {
     const canvas = document.createElement('canvas');
     const ctx = canvas.getContext('2d');
+
     const isSquare = image.width === image.height;
-    const resolutions = isSquare ? squareResolutions : rectangularResolutions;
+    const resolutions = check() ? construct3Icons : isSquare ? squareResolutions : rectangularResolutions;
+
     for (const resolution of resolutions) {
         const [width, height] = resolution;
         canvas.width = width;
         canvas.height = height;
         ctx.drawImage(image, 0, 0, width, height);
-        const resizedImage = canvas.toDataURL('image/jpeg', 0.92); //isSquare ? 1.0 : 0.6
+        
+        // Use a qualidade especificada ao salvar a imagem
+        const resizedImage = canvas.toDataURL('image/jpeg', 0.92);
+
         const imageItem = document.createElement('li');
-        // Crie um contêiner para o texto da resolução e o botão "Salvar"
-        const resolutionContainer = document.createElement('div');
-        // Adicione o texto da resolução ao contêiner
         const resolutionText = document.createElement('span');
-        resolutionText.textContent = `${width} x ${height}` + ' ';
-        resolutionContainer.appendChild(resolutionText);
-        // Adicione um botão "Salvar" ao contêiner
+        resolutionText.textContent = `${width} x ${height}`;
+        imageItem.appendChild(resolutionText);
         const saveButton = document.createElement('button');
+        
         saveButton.textContent = 'Salvar';
         saveButton.addEventListener('click', () => {
             const link = document.createElement('a');
@@ -28,16 +31,15 @@ function resizeImage(image, squareResolutions, rectangularResolutions) {
             link.download = `${width}x${height}.jpg`;
             link.click();
         });
-        resolutionContainer.appendChild(saveButton);
-        imageItem.appendChild(resolutionContainer);
-        // Adicione a imagem redimensionada abaixo do contêiner
+        
+        imageItem.appendChild(saveButton);
         const imageElement = document.createElement('img');
         imageElement.src = resizedImage;
         imageItem.appendChild(imageElement);
+        
         document.querySelector('#image-list').appendChild(imageItem);
     }
 }
-
 
 document.querySelector('#image-input').addEventListener('change', event => {
     const file = event.target.files[0];
@@ -52,9 +54,18 @@ document.querySelector('#image-input').addEventListener('change', event => {
 
 // Adicione um manipulador de eventos ao botão "Limpar"
 document.querySelector('#clear-button').addEventListener('click', () => {
+    clearElements();
+});
+
+const clearElements = () => {
     // Remova todas as imagens redimensionadas da página
     const imageList = document.querySelector('#image-list');
     while (imageList.firstChild) {
         imageList.removeChild(imageList.firstChild);
     }
-});
+}
+
+const check = () => {
+    var checkbox = document.getElementById("checkbox");
+    return checkbox.checked;
+}
