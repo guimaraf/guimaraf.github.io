d051ff24c2270af4cefc05760ac084af0640eb4b
guimaraf <guimaraf@gmail.com>
2025-12-09 11:20:13 -0300

    add new game test

---
+++ works/novo/scripts/project/javaScriptInEvents.js
@@ -0,0 +1,16 @@
+
+
+const scriptsInEvents = {
+
+	async Gameplay_Event8_Act11(runtime, localVars)
+	{
+		console.log("salvou meu ovo");
+	},
+
+	async Gameplay_Event11_Act5(runtime, localVars)
+	{
+		console.log("carregou meu ovo");
+	}
+};
+
+globalThis.C3.JavaScriptInEvents = scriptsInEvents;
